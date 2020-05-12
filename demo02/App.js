5f47b24f130fd102d69706d8495a562e6d407af6
yuwangjuan <1183910959@qq.com>
2020-05-12 09:43:44 +0800

    node内置模块

--- demo02/App.js
+++ demo02/App.js
@@ -1,7 +1,15 @@
-// node-http-server
-// var http = require('http');
-// http.createServer(function(request,response) {
-//     response.writeHead(200,{'Content-Type':'text/plain'});
-//     response.end("Hello World");
-// }).listen(8081);
-// console.log("Server running at http://127.0.0.1:8081");
+const http = require('http');
+const url =require('url');
+http.createServer(function(req,res){
+    console.log(req.url,'res');
+    // console.log(req.url);//获取url;
+    res.writeHead(200,{"content-type":"text/html;charset='utf-8'"});//解决乱码
+    res.write("<head><meta charset='utf-8'></head>")//解决乱码
+    // res.write('this is nodejs');
+    // res.write('<h2>你好 nodeJS</h2>')
+    if(req.url !='/favicon.ico'){
+        var userInfo = url.parse(req.url,true).query;
+        console.log(`姓名：${userInfo.name}---年龄${userInfo.age}`);
+    }
+    res.end();
+}).listen(3000);
---
+++ demo02/url.js
@@ -0,0 +1,7 @@
+const url =require('url');
+var api ='http://www.itying.com?name=zhangshan&age=20';
+console.log(url.parse(api,true));
+
+var getValue = url.parse(api,true).query;
+console.log(getValue);
+console.log(`姓名：${getValue.name}---年龄${getValue.age}`);
